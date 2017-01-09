5ed370991d9e057ff21b883d82307d5c98e2180a
KIKU <kikuno-yuki@av.avex.co.jp>
2017-01-09 05:38:01 +0000

    04_実習

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'books/index'
 
   devise_for :users
+  
+    if Rails.env.development?
+      mount LetterOpenerWeb::Engine, at: "/letter_opener"
+    end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
