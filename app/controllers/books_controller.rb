7c3f80828d0c045261017b25fe2da3d632ef99f9
KIKU <kikuno-yuki@av.avex.co.jp>
2017-01-15 10:28:13 +0000

    10_実習

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,21 +2,26 @@ class BooksController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   
   def index
-    @books = Book.includes(:bookmarks, :reviews).order('updated_at DESC')
+    if user_signed_in? && params[:ft] && params[:ft] == 'my'
+      @books = Book.includes(:bookmarks, :reviews, :user).where(user_id: current_user.id).order('updated_at DESC')
+    elsif user_signed_in? && params[:ft] && params[:ft] == 'bookmark'
+      @books = Book.joins(:bookmarks).where('bookmarks.user_id = ?', current_user.id).order('updated_at DESC')
+    else
+      @books = Book.includes(:bookmarks, :reviews, :user).order('updated_at DESC')
+    end
   end
   
   def show
     @book = Book.find(params[:id])
-      if user_signed_in?
-        @my_bookmark = @book.bookmarks.select{|s| s.user_id == current_user.id}.first
+    if user_signed_in?
+      @my_bookmark = @book.bookmarks.select{|s| s.user_id == current_user.id}.first
+    end
+    if user_signd_in?
+      my_review = @book.reviews.select{|s| s.user_id == current_user.id}.first
+      unless my_review
+        @my_review = Review.new
       end
-      
-      if user_signed_in?
-        my_review = @book.reviews.select{|s| s.user_id == current_user.id}.first
-        unless my_review
-          @my_review = Review.new
-        end
-      end
+    end
   end
   
   def new
