9976e73c333497a3efa4c3e5f8da9ffead16b74d
KIKU <kikuno-yuki@av.avex.co.jp>
2017-01-15 13:34:11 +0000

    11_基本課題2

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,6 +1,6 @@
 module ReviewsHelper
   
   def review_average(reviews)
-    reviews.length > 0.0 ? reviews.inject(0.0) {|sum, r| sum + r.rank} / reviews.length : 0.0
+    (reviews.length > 0.00 ? reviews.inject(0.00) {|sum, r| sum + r.rank} / reviews.length : 0.00).round(2)
   end
 end
